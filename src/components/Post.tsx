26cb930f3e86c165ad34ea5ebabddac45d149b99
Aristiklever R. Sousa <aristikleverrodrigues@gmail.com>
2024-01-11 20:13:36 -0300

    ✨ feat: form de comentário

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -32,6 +32,16 @@ export function Post({ author, content }: { author: Author, content: string }) {
           <a href="">#rocketseat</a>{' '}
         </p>
       </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+
+        <textarea
+          placeholder='Deixe um comentário'
+        />
+
+        <footer><button type='submit'>Publicar</button></footer>
+      </form>
     </article>
   );
 }
